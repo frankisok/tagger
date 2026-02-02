740868074c871cd5ddf2afaaf94f508c37122284
Frank <frank@clawd.bot>
2026-02-02 19:28:23 +0000

    Initial: Javalin + Maven setup

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(8080);
+
+        app.get("/", ctx -> ctx.result("Hello from Tagger!"));
+    }
+}
